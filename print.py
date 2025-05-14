37af716dbb3fac6b9d8f68dd572f49219e0c457c
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-14 10:22:28 +0530

    keywords,prints and variables

---
+++ print.py
@@ -0,0 +1,5 @@
+print("hello world")
+print(5)
+print("this is a good day \n")
+print("I am happy",7)
+print("I am sad", end="I am not sad")
---
+++ variables.py
@@ -0,0 +1,5 @@
+number = 4
+start = "hello world"
+print(number,start)
+name = input("what is your name")
+print(name)
