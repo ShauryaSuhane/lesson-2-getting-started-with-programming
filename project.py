f4fe230075f9813b3eeb89d941eb2f376b82a7c6
ShauryaSuhane <suhaneshaurya26@gmail.com>
2025-05-14 10:31:20 +0530

    homework

---
+++ project.py
@@ -0,0 +1,8 @@
+shaurya = "26 september"
+mother = "11 september"
+father = "17 july"
+sister = "1 june"
+print("my birthday is on", shaurya)
+print("mother's birthday is on", mother)
+print("father's birthday is on", father)
+print("sister's birthday is on", sister)
